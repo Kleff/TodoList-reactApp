f908de847150f098a22075308060d49e243320ef
Kleff <julian.scerra@gmail.com>
2019-01-07 16:51:16 -0300

    Updates

---
+++ precache-manifest.038d673df5ac3b008c55b792c20f2b79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f151b14455f92361abf0",
+    "url": "/static/css/main.48e29942.chunk.css"
+  },
+  {
+    "revision": "f151b14455f92361abf0",
+    "url": "/static/js/main.f151b144.chunk.js"
+  },
+  {
+    "revision": "5ef3b90050b60f0bfacb",
+    "url": "/static/js/1.5ef3b900.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8e4b302273e4ed2115af167c2a4ac561",
+    "url": "/index.html"
+  }
+];
